bc4a1a7e82e9ee5fce8ecec98bdddb4070fbd4a8
Modesto Amador <mofesdo.dev@gmail.com>
2023-11-22 23:37:20 -0800

    arrays worldwide cheersmack done

--- class19/class19-materials/arrays-worldwide-cheersmack/js/main.js
+++ class19/class19-materials/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,9 @@
 //Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+function count(num){
+    let arr = []
+    for(let i = 0; i <=num; i++){
+        arr[i] = i
+    }
+    return arr
+}
+console.log(count(80))
