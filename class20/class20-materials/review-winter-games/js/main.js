85c80ea96bfcb54a1364016f673766f36efc1e9d
Modesto Amador <mofesdo.dev@gmail.com>
2023-11-25 13:31:54 -0800

    review winter games done

--- class20/class20-materials/review-winter-games/js/main.js
+++ class20/class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,14 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers. 
+//Return a new array containing every even number from the original array (do not use map or filter)
+
+function evenNums(arr){
+    let result = []
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 == 0){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+let test = [1,2,3,4,5,6,7,8,9,10]
+console.log(evenNums(test))
