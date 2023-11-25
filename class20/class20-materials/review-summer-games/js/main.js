914c4a374b099e515311b8f3ccb6c5be60f391e0
Modesto Amador <mofesdo.dev@gmail.com>
2023-11-25 13:03:08 -0800

    review summer games done

--- class20/class20-materials/review-summer-games/js/main.js
+++ class20/class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,11 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multEach(arr){
+    let product = 1;
+    for(let i = 0; i < arr.length; i++){
+        product = product * arr[i]
+    }
+    alert(product)
+}
+
+let nums = [2, 3, 10]
+multEach(nums)
