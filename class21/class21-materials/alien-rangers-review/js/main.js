054ecbae272154756b6bb94574e9248e757c3189
Modesto Amador <mofesdo.dev@gmail.com>
2023-11-28 15:27:17 -0800

    might morphin review done

--- class21/class21-materials/alien-rangers-review/js/main.js
+++ class21/class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,25 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
-
+let shows = ["Regular Show", "Rick & Morty", "Bob's Burgers"]
+shows.forEach(element => {
+  console.log(element)  
+});
 //Create and array of numbers
+let nums = [2, 5, 3, 4, 1]
 //Return a new array of numbers that includes every even number from the previous Arrays
-
+let evenNums = []
+for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 0){
+        evenNums.push(nums[i])
+    }
+}
+console.log(evenNums)
 //Create a function that takes in an array of numbers
+function secondBest(arr){
+    arr.sort()
+    console.log(arr)
+    alert(arr[1] + arr[arr.length-2])
+}
+secondBest(nums)
 //Alert the sum of the second lowest and the second highest number
--- class21/class21-materials/mighty-morphin-review/js/main.js
+++ class21/class21-materials/mighty-morphin-review/js/main.js
@@ -1,18 +1,58 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
-
+let holiday = "Christmas";
+holiday = holiday.toUpperCase();
+console.log(holiday)
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
-
+let str = "Hello";
+alert(str.slice(-3))
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
-
-
+function subFive(num1, num2, num3, num4, num5){
+    alert(Math.abs(100 - num1 - num2 - num3 - num4 - num5))
+}
+subFive(10, 20, 5, 5, 100)
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
-
-
+function lowHigh(num1, num2, num3){
+    if(num1 < num2 && num1 < num3){
+        console.log(num1)
+    }
+    if(num2 < num1 && num2 < num3){
+        console.log(num2)
+    }
+    if(num3 < num1 && num3 < num2){
+        console.log(num3)
+    }
+    if(num1 > num2 && num1 > num3){
+        console.log(num1)
+    }
+    if(num2 > num1 && num2 > num3){
+        console.log(num2)
+    }
+    if(num3 > num1 && num3 > num2){
+        console.log(num3)
+    }
+}
+lowHigh(3, 5, 2)
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
-
-
+function coinFlip(){
+    let random = Math.random();
+    if(random < .5){
+        return "heads"
+    }
+    else{
+        return "tails"
+    }
+}
+console.log(coinFlip())
 //*Loops*
-//Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
+//Create a function that takes in a number. Console log the result of heads or tails using the previous 
+//function x times where x is the number passed into the function. Call the function.
+function coinFlipXTimes(num){
+    for(let i = 0; i < num; i++){
+        console.log(coinFlip(), i)
+    }
+}
+
+coinFlipXTimes(50)
