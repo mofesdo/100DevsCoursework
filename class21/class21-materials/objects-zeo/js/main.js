b01e51047e36545aad4c774e19412a7bbac2c33b
Modesto Amador <mofesdo.dev@gmail.com>
2023-11-28 17:05:58 -0800

    objects zeo done

--- class21/class21-materials/objects-zeo/js/main.js
+++ class21/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,19 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {}
+
+stopwatch.currentTime = 12;
+stopwatch.color = "black";
+stopwatch.brand = "Gucci";
+stopwatch.screen ="led";
+
+stopwatch.tellTime = function(){
+    console.log(stopwatch.currentTime)
+}
+
+stopwatch.tellColor = function(){
+    console.log(stopwatch.color)
+}
+
+stopwatch.flex = function(){
+    console.log(stopwatch.brand)
+}
