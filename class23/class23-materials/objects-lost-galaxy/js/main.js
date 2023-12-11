552e5f4b9ef681448a4329227ba023ffc091c9a7
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-10 21:57:46 -0800

    objects lost galaxy done

--- class23/class23-materials/objects-lost-galaxy/js/main.js
+++ class23/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,22 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {}
+
+dog.breed = "pug"
+dog.age = "4"
+dog.name = "lola"
+dog.stinky = true;
+
+dog.snore = function(){
+    console.log("Zzzzz")
+}
+
+dog.bark = function(){
+    console.log("Woooof!")
+}
+
+dog.takeBath = function(dog){
+    if(dog.stinky == true){
+        console.log("dog needs a bath")
+    }
+}
