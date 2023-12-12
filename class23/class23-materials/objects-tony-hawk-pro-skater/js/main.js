c1a271a9175a2bb7663fb3864cee3732d7da09be
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-12 14:20:38 -0800

    tony hawk pro skater done

--- class23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,20 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function MakeSkate(board, stance, push, style){
+    this.board = board;
+    this.stance = stance;
+    this.push = push;
+    this.style = style;
+
+    this.ollie = function(){
+        console.log("ollied over the curb")
+    }
+
+    this.bail = function(){
+        console.log("oh FUUUUUUU...")
+    }
+    this.kickflip = function(){
+        alert("siiiick! you did a kickflip")
+    }
+}
+
+let modesto = new MakeSkate("local", "goofy", "regular", "street")
