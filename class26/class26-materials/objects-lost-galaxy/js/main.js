b7e58ef0e3ac6ef546fa9910857029439322a880
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-16 22:36:16 -0800

    dog object

--- class26/class26-materials/objects-lost-galaxy/js/main.js
+++ class26/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,13 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {};
+
+dog.breed = "pug"
+dog.age = "4"
+dog.size = "small"
+dog.stinky = true;
+
+dog.pee = function(){
+    console.log("psssssss")
+}
+
