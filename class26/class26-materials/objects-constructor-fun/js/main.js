b76fa6972abfad29a36df9be2b7558f8821e4037
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-17 22:31:00 -0800

    created constructor with 4 properties.

--- class26/class26-materials/objects-constructor-fun/js/main.js
+++ class26/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,10 @@
 //Create a constructor with 4 properties and 3 methods
+
+function MakePhone(brand, color, provider, price){
+    this.brand = brand;
+    this.color = color;
+    this.provider = provider;
+    this.price = price;
+
+    
+}
