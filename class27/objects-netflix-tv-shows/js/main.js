d219150ccbbf0ec6bd7cd23d492863b619ffe3ea
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-20 21:23:08 -0800

    objects netflix show done.

--- class27/objects-netflix-tv-shows/js/main.js
+++ class27/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,20 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class MakeNetflixShow{
+    constructor(name, genre, audience, language){
+        this.name = name;
+        this.genre = genre;
+        this.audience = audience;
+        this.language = language
+    }
 
+    play(){
+        console.log("the show has started!")
+    }
+    pause(){
+        alert("The show has been paused!")
+    }
+    rate(num){
+        console.log(`You rated this show ${num} stars!`)
+    }
+}
