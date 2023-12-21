13023abc4e7a861b9913ca08b57aa3b174cf8801
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-20 21:30:42 -0800

    more appropriate properties for tv shows

--- class27/objects-netflix-tv-shows/js/main.js
+++ class27/objects-netflix-tv-shows/js/main.js
@@ -1,15 +1,15 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
 class MakeNetflixShow{
-    constructor(name, genre, audience, language){
-        this.name = name;
+    constructor(title, genre, rating, numOfEp){
+        this.title = title;
         this.genre = genre;
-        this.audience = audience;
-        this.language = language
+        this.rating = rating;
+        this.numOfEp = numOfEp
     }
 
     play(){
-        console.log("the show has started!")
+        console.log("Playing")
     }
     pause(){
         alert("The show has been paused!")
