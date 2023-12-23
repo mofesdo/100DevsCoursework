31404653ef8e0fd629fae6e659c13168734c17fb
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-22 22:20:02 -0800

    getting nasa potd

--- class27/api-template/js/main.js
+++ class27/api-template/js/main.js
@@ -3,12 +3,16 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
-
+  const url = `https://api.nasa.gov/planetary/apod?api_key=dGFFAU3XpADV7pM3g2Xt2c4AsHALFfhQTwdQIWIb&date=${choice}`
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        if(data.media_type === 'image'){
+          document.querySelector('img').src = data.hdurl
+        } else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+        }
       })
       .catch(err => {
           console.log(`error ${err}`)
