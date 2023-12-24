dd206310a3be6ee952671e2ac4a2601c80fe01ea
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-23 16:46:05 -0800

    trying to increment score

--- class27/local-storage-botScore-button/js/main.js
+++ class27/local-storage-botScore-button/js/main.js
@@ -1,4 +1,11 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
 localStorage.setItem("score", 0)
-document.querySelector("h2").addEventListener("click", incrementScore());
+document.querySelector("h2").addEventListener("click", incrementScore())
+
+incrementScore(){
+    increment = localStorage.getItem("score") + 1;
+    localStorage.setItem("score", increment)
+}
+
+console.log(localStorage.getItem("score"))
