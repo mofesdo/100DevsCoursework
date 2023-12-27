a91b6dfc4682f46afe52e0e9d4a0eb92df902efe
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-26 22:18:00 -0800

    added reset score function

--- class27/local-storage-botScore-button/js/main.js
+++ class27/local-storage-botScore-button/js/main.js
@@ -2,9 +2,10 @@
 if(localStorage.getItem("score") == null){
     localStorage.setItem("score", 0)
 }
+document.querySelector("h2").innerText = "Score: " + Number(localStorage.getItem("score"))
 document.querySelector('.increment').addEventListener('click', incrementScore)
 document.querySelector('.decrement').addEventListener('click', decrementScore)
-
+document.querySelector('.reset').addEventListener('click', resetScore)
 function incrementScore(){
     increment = Number(localStorage.getItem("score")) + 1;
     localStorage.setItem("score", increment)
@@ -15,5 +16,10 @@ function decrementScore(){
     decrement = Number(localStorage.getItem("score")) + -1;
     localStorage.setItem("score", decrement)
     document.querySelector("h2").innerText = "Score: " + decrement
+}
+
+function resetScore(){
+    localStorage.setItem("score", 0)
+    document.querySelector("h2").innerText = "Score: " + Number(localStorage.getItem("score"))
 }
 console.log(localStorage.getItem("score"))
