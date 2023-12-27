87fcfad20a74b49f461fefc76a0437615e747bcb
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-26 22:11:42 -0800

    added decrement function

--- class27/local-storage-botScore-button/js/main.js
+++ class27/local-storage-botScore-button/js/main.js
@@ -2,11 +2,18 @@
 if(localStorage.getItem("score") == null){
     localStorage.setItem("score", 0)
 }
-document.querySelector('button').addEventListener('click', incrementScore)
+document.querySelector('.increment').addEventListener('click', incrementScore)
+document.querySelector('.decrement').addEventListener('click', decrementScore)
 
 function incrementScore(){
     increment = Number(localStorage.getItem("score")) + 1;
     localStorage.setItem("score", increment)
+    document.querySelector("h2").innerText = "Score: " + increment
 }
 
+function decrementScore(){
+    decrement = Number(localStorage.getItem("score")) + -1;
+    localStorage.setItem("score", decrement)
+    document.querySelector("h2").innerText = "Score: " + decrement
+}
 console.log(localStorage.getItem("score"))
