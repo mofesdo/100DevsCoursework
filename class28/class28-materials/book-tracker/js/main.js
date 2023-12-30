d6990ee3f54ebd1da176d31b5cb3d1066968e6c2
Modesto Amador <mofesdo.dev@gmail.com>
2023-12-30 12:10:03 -0800

    added books read to local storage

--- class28/class28-materials/book-tracker/js/main.js
+++ class28/class28-materials/book-tracker/js/main.js
@@ -1,6 +1,7 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
+
 function getFetch(){
   const isbn = document.querySelector('input').value
   console.log(isbn)
@@ -10,6 +11,8 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data.title)
+        localStorage.setItem("books", data.title);
+        document.querySelector(".books").innerHTML += localStorage.getItem("books") + " ";
 
       })
       .catch(err => {
