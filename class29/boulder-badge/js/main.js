4bd39af5d4e05f1b0ea0c90a182cd7543ff63f09
Modesto Amador <mofesdo.dev@gmail.com>
2024-01-04 16:09:27 -0800

    boulder badge done

--- class29/boulder-badge/js/main.js
+++ class29/boulder-badge/js/main.js
@@ -1,8 +1,40 @@
 // Variables
-//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
+//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. 
+//You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. 
+//Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). 
+//Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
+let bulbCandy = 16 -5;
+let catCandy = 7 - 1;
+let weedCandy = 7 - 1;
+
+let totalCandies = bulbCandy + catCandy + weedCandy;
+console.log(totalCandies)
 
 //Conditionals And Functions
-//You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+//You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. 
+//Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+function charmanderBattle(degree){
+    if(degree > 0){
+        console.log("charmander can battle!")
+    }
+    else{
+        console.log("charmander cannot battle!")
+    }
+}
 
+function fahrenheitToCelcius(degree){
+    return (degree - 32) * 5/9
+}
+console.log(fahrenheitToCelcius(120))
+console.log(charmanderBattle(2))
 //Loops
-//You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
+//You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. 
+//Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
+
+function pikachu(trainerPKM){
+    for(let i = 0; i < trainerPKM; i++){
+        console.log("Pikachu I choose you")
+    }
+}
+
+console.log(pikachu(6))
