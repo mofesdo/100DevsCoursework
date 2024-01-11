1ef957837e25de89cf75de411c1c02347974db14
Modesto Amador <mofesdo.dev@gmail.com>
2024-01-10 21:03:43 -0800

    filter on array to return vals that are multiples of their index

--- class29/cascade-badge/js/main.js
+++ class29/cascade-badge/js/main.js
@@ -38,4 +38,13 @@ console.log(biggerthan(a, b))
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
+let array1 = [68, -1, 1, -7, 10, 10]
+const multiple = array1.filter((x, index) => {
+    if(x % index == 0){
+        return x;
+    }
+    return ;
+})
+console.log(multiple)
+
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
