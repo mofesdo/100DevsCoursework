4d35abb0e6a815b0fbf5e28457d7f6f0fac126e9
Modesto Amador <mofesdo.dev@gmail.com>
2024-01-10 20:41:09 -0800

    biggerthan array done

--- class29/cascade-badge/js/main.js
+++ class29/cascade-badge/js/main.js
@@ -10,7 +10,29 @@ for(let i = party.length -1; i >= 0; i--){
 reverseParty(["pikachu", "rowlet", "dialga", "murkrow", "charmander", "rayquaza"])
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element 
 //in a is strictly greater than the sum of the cubes of each element in b.
+function biggerthan(a, b){
+    let sumA = 0;
+    let sumB = 0;
 
+    for(let i = 0; i < a.length; i++){
+        sumA += a[i];
+    }
+    for(let i = 0; i < b.length; i++){
+        sumB += b[i];
+    }
+
+    if(sumA > sumB){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let a = [2, 3, 4, 5, 100]
+let b = [1, 3, 4, 5, 8]
+
+console.log(biggerthan(a, b))
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
